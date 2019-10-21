1ffadeb69897af5b538ca0ec8b43cd12a6e7e842
Patryk Cyrek <patryk.cyrek@yahoo.com>
2019-10-21 17:02:06 +0200

    Initial Commit

---
+++ iOS2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS2
+//
+//  Created by Patryk Cyrek on 21/10/2019.
+//  Copyright © 2019 Patryk Cyrek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
